2be0120990c5187c66e7e784a06a6dae218c6fed
TrungTruc <trungtruc1094@gmail.com>
2018-01-24 11:30:14 +0700

    Init project

---
+++ AttendanceSystem/AttendanceSystem/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AttendanceSystem
+//
+//  Created by TamTran on 1/24/18.
+//  Copyright © 2018 TamTran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AttendanceSystem/AttendanceSystem/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AttendanceSystem
+//
+//  Created by TamTran on 1/24/18.
+//  Copyright © 2018 TamTran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
